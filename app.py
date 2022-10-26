3824cc5162cc4295d8a61d9ca6a708bf94546d1c
Tejus Gupta <109463410+tejus3131@users.noreply.github.com>
2022-10-26 10:46:28 +0530

    just chill

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route("/harsh")
+def harsh_video():
+    return render_template("harsh.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
