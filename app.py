786a690b14af7390c58b26803cb67af9fdaa98de
Tejus Gupta <109463410+tejus3131@users.noreply.github.com>
2022-10-26 14:27:12 +0530

    base template

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from re import T
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+
+
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
 
-@app.route("/harsh")
-def harsh_video():
-    return render_template("harsh.html")
+
+@app.route("/blogs/<type>")
+def blogs(type):
+    return render_template("blogs.html",blogs = type)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
