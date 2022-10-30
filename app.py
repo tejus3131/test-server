890208f9347559698e7442f5295e60c7a8d97aa0
Tejus Gupta <109463410+tejus3131@users.noreply.github.com>
2022-10-30 21:04:08 +0530

    template commit

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-
+Title = "TheHarshBlogs"
 
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html", title = Title)
 
 
 @app.route("/blogs/<type>")
 def blogs(type):
-    return render_template("blogs.html",blogs = type)
+    return render_template("blogs.html", title = Title, blogs = type)
 
 
 if __name__ == "__main__":
